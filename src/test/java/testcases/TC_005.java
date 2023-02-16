4a95d202482a661f39367c8529c5c3bae49418a0
mohd <mohammed@gmail.com>
2023-02-15 20:41:46 -0500

    updated changes

--- src/test/java/testcases/TC_005.java
+++ src/test/java/testcases/TC_005.java
@@ -4,7 +4,9 @@ public class TC_005 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("TC_005");			
+		System.out.println("TC_005");
+		
+		System.out.println("Hello..Welcome");
 	}
 
 }
